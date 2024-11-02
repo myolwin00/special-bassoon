b49ce22f2d4618cd9c5cf12e953b6d3ee1dee314
Emrys <myolwinoo919@gmail.com>
2024-11-03 02:31:36 +0700

    added 'react-native-safe-area-context'

--- App.js
+++ App.js
@@ -1,20 +1,26 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { SafeAreaView } from 'react-native-safe-area-context';
 
-export default function App() {
+const App = () => {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaView style={styles.container}>
+      <View>
+        <StatusBar style="auto" />
+        <Text>Open up App.js to start working on your app!!!!!!!!!!!</Text>
+        <Text>Open up App.js to start working on your app!!!!!!!!!!!</Text>
+      </View>
+    </SafeAreaView>
   );
 }
+
+export default App;
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: 'blue',
+    // alignItems: 'center',
+    // justifyContent: 'center',
   },
 });
